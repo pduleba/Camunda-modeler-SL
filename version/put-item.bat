c911f3f022c3a9344a95970ba6e73a3a654a8046
pduleba <pduleba@pgs-soft.com>
2019-02-28 15:03:50 +0100

    Multiple versions

---
+++ version/put-item.bat
@@ -0,0 +1,1 @@
+curl -H "Content-Type: application/json" -X POST -d "{\"variables\": {\"amount\": {\"value\":555,\"type\":\"long\"}, \"item\": {\"value\":\"item-xyz\"} } }" http://localhost:8080/engine-rest/process-definition/key/payment-retrieval/start
